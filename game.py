8b5d7aaa69b4c0c80f53accdf857da638d902da0
Marma <elbaz.amram@gmail.com>
2016-07-11 14:19:39 +0200

    first try of a game file application.

---
+++ game.py
@@ -0,0 +1,22 @@
+!/usr/bin/python3
+
+import DungeonRPG
+
+if __name__ == "__main__":
+    #character initialization:
+    char            = "X"
+    char_position   = [1, 1]
+    treasure        = "#"
+    n_levels_total  = 20
+    data = {
+        "gc"    :   0,
+        "hp"    :   25,
+        "level" :   None
+    }
+
+    #game launch
+    for n_level in range(1, n_levels_total + 1):
+        level = DungeonRPG.load_labyrinth("level_" + str(n_level))
+        data["level"] = n_level
+        DungeonRPG.game(level, data, char, char_position, treasure)
+    print("YOU WIN !!!")
